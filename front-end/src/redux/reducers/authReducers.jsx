b4b848609bda39c8f14f9318582842e3751cea9b
Phanttoms <yohan.pruche@orange.fr>
2024-05-02 14:49:33 +0200

    test

---
+++ front-end/src/redux/reducers/authReducers.jsx
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	isLoggedIn: false,
+	token: localStorage.getItem("token"),
+	user: null,
+	error: null,
+};
+
+const authSlice = createSlice({
+	name: "UserAuth",
+	initialState,
+	reducers: {
+		loginSuccess(state, action) {
+			state.isLoggedIn = true;
+			state.user = action.payload;
+			state.token = action.payload.token;
+			state.error = null;
+		},
+		loginFailure(state, action) {
+			state.isLoggedIn = false;
+			state.user = null;
+			state.token = null;
+			state.error = action.payload;
+		},
+		logout(state) {
+			state.isLoggedIn = false;
+			state.user = null;
+			state.token = null;
+			state.error = null;
+			localStorage.removeItem("token");
+		},
+	},
+});
+
+export const { loginSuccess, loginFailure, logout } = authSlice.actions;
+export default authSlice.reducer;

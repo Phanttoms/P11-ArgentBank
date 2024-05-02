1bd221c00bb6d16d0e951a74e9b18a89d74aeda3
Phanttoms <yohan.pruche@orange.fr>
2024-05-02 12:37:52 +0200

    ajout Form

---
+++ front-end/src/components/LoginForm/index.jsx
@@ -0,0 +1,29 @@
+import "../LoginForm/_loginForm.scss";
+
+export default function LoginForm() {
+	return (
+		<section className="login">
+			<i className="fa fa-user-circle login__icon"></i>
+			<h1>Sign In</h1>
+			<form className="login__form">
+				<div className="login__form--wrapper">
+					<label htmlFor="username">Username</label>
+					<input type="text" id="username" />
+				</div>
+				<div className="login__form--wrapper">
+					<label htmlFor="password">Password</label>
+					<input type="password" id="password" />
+				</div>
+				<div className="login__form--remember">
+					<input type="checkbox" id="remember-me" />
+					<label htmlFor="remember-me">Remember me</label>
+				</div>
+				{/* En attendant d'ajouter la logic de form */}
+				<a href="/" className="login__form--button">
+					Sign In
+				</a>
+				{/* <button className="login__form--button">Sign In</button> */}
+			</form>
+		</section>
+	);
+}
--- front-end/src/pages/Login/Login.jsx
+++ front-end/src/pages/Login/Login.jsx
@@ -1,5 +1,6 @@
 // Components
 import Header from "../../components/Header";
+import Form from "../../components/LoginForm";
 import Footer from "../../components/Footer";
 
 export default function Home() {
@@ -7,25 +8,7 @@ export default function Home() {
 		<>
 			<Header />
 			<main className="main bg-dark">
-				<section className="sign-in-content">
-					<i className="fa fa-user-circle sign-in-icon"></i>
-					<h1>Sign In</h1>
-					<form>
-						<div className="input-wrapper">
-							<label htmlFor="username">Username</label>
-							<input type="text" id="username" />
-						</div>
-						<div className="input-wrapper">
-							<label htmlFor="password">Password</label>
-							<input type="password" id="password" />
-						</div>
-						<div className="input-remember">
-							<input type="checkbox" id="remember-me" />
-							<label htmlFor="remember-me">Remember me</label>
-						</div>
-						<button className="sign-in-button">Sign In</button>
-					</form>
-				</section>
+				<Form />
 			</main>
 			<Footer />
 		</>

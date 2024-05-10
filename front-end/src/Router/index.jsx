60f3da41a63e789a64d5c0ab87997a3f7b74ef37
Phanttoms <yohan.pruche@orange.fr>
2024-05-10 19:18:41 +0200

    ajout Persist, modif store

--- front-end/src/Router/index.jsx
+++ front-end/src/Router/index.jsx
@@ -4,18 +4,21 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import User from "../pages/User";
 import { Provider } from "react-redux";
-import store from "../redux/store";
+import { store, persistor } from "../redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function Router() {
 	return (
 		<Provider store={store}>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="login" element={<Login />} />
-					<Route path="user" element={<User />} />
-				</Routes>
-			</BrowserRouter>
+			<PersistGate loading={null} persistor={persistor}>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="login" element={<Login />} />
+						<Route path="user" element={<User />} />
+					</Routes>
+				</BrowserRouter>
+			</PersistGate>
 		</Provider>
 	);
 }
--- front-end/src/redux/store.jsx
+++ front-end/src/redux/store.jsx
@@ -1,12 +1,44 @@
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+
 import { configureStore } from "@reduxjs/toolkit";
 import authReducers from "../redux/reducers/authReducers.jsx";
 import profileReducers from "../redux/reducers/profileReducers.jsx";
 
-const store = configureStore({
-	reducer: {
+const persistConfig = {
+	key: "root",
+	version: 1,
+	storage,
+};
+
+const persistedReducer = persistReducer(
+	persistConfig,
+	combineReducers({
 		auth: authReducers,
 		profile: profileReducers,
-	},
+	})
+);
+
+const store = configureStore({
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export { store, persistor };

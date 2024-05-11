f971ed1f17b53531ca95c844f29dbd8c602841c5
Phanttoms <yohan.pruche@orange.fr>
2024-05-11 16:09:55 +0200

    modif session storage

--- front-end/src/redux/store.jsx
+++ front-end/src/redux/store.jsx
@@ -8,7 +8,7 @@ import {
 	PURGE,
 	REGISTER,
 } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import sessionStorage from "redux-persist/lib/storage/session";
 import { combineReducers } from "redux";
 
 import { configureStore } from "@reduxjs/toolkit";
@@ -18,7 +18,7 @@ import profileReducers from "../redux/reducers/profileReducers.jsx";
 const persistConfig = {
 	key: "root",
 	version: 1,
-	storage,
+	storage: sessionStorage,
 };
 
 const persistedReducer = persistReducer(

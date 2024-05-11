a318a161a88f28bac5372582677adbd498b9e4b5
Phanttoms <yohan.pruche@orange.fr>
2024-05-12 01:24:38 +0200

    ajout gestion erreur 404

--- front-end/src/Router/index.jsx
+++ front-end/src/Router/index.jsx
@@ -3,6 +3,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import User from "../pages/User";
+import Error from "../pages/Error";
 import { Provider } from "react-redux";
 import { store, persistor } from "../redux/store";
 import { PersistGate } from "redux-persist/integration/react";
@@ -16,6 +17,7 @@ export default function Router() {
 						<Route path="/" element={<Home />} />
 						<Route path="login" element={<Login />} />
 						<Route path="user" element={<User />} />
+						<Route path="*" element={<Error />} />
 					</Routes>
 				</BrowserRouter>
 			</PersistGate>
---
+++ front-end/src/pages/Error/index.jsx
@@ -0,0 +1,13 @@
+import "../Error/_error.scss";
+
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+export default function Error() {
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
+}

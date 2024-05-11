1da11cbf85d7e62b951b24498b071c6c7cebee4d
Phanttoms <yohan.pruche@orange.fr>
2024-05-12 01:31:00 +0200

    ajout du style de la page Error

--- front-end/src/pages/Error/index.jsx
+++ front-end/src/pages/Error/index.jsx
@@ -3,10 +3,21 @@ import "../Error/_error.scss";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
+import { Link } from "react-router-dom";
+
 export default function Error() {
 	return (
 		<>
 			<Header />
+			<div className="error">
+				<h1 className="error__title">404</h1>
+				<p className="error__txt">
+					Oups! La page que vous demandez n'existe pas.
+				</p>
+				<Link to="/" className="error__return">
+					Retourner sur la page d'accueil
+				</Link>
+			</div>
 			<Footer />
 		</>
 	);

e1836b311998605f563b50fceacc212c60853ee5
Phanttoms <yohan.pruche@orange.fr>
2024-05-20 13:17:06 +0200

    modif image Banner

--- front-end/src/components/Banner/index.jsx
+++ front-end/src/components/Banner/index.jsx
@@ -1,10 +1,17 @@
 import "../Banner/_banner.scss";
 import banner from "../../assets/images/bank-tree.webp";
+import bannerTablette from "../../assets/images/bank-tree-tablette.webp";
+import bannerMobile from "../../assets/images/bank-tree-mobile.webp";
 
 export default function Banner() {
 	return (
 		<div className="banner">
-			<img src={banner} alt="bank-tree" className="banner__img" />
+			<picture>
+				<source srcSet={bannerMobile} media="(max-width: 780px)" />
+				<source srcSet={bannerTablette} media="(max-width: 1000px)" />
+				<img src={banner} alt="bank-tree" className="banner__img" />
+			</picture>
+
 			<section className="banner__content">
 				<h2 className="banner__content--title">Promoted Content</h2>
 				<p className="banner__content--subtitle">No fees.</p>
